5cf64856dac16cfb0353a7221f7f7200dc421d58
Ivy <yunjilim@gmail.com>
2019-08-13 13:32:10 -0700

    login

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
@@ -13,5 +15,9 @@ router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+router.post('/register', userController.validateRegister, catchErrors(userController.register), authController.login);
 
 module.exports = router;
